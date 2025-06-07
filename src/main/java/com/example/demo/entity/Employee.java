892eb5c9ae9f136c51fa89a1fb1a06fb2e48c5e4
mutyala517 <mutyala.dharmendra@gmail.com>
2025-06-07 17:54:48 -0500

    Removing unused dependencies

--- src/main/java/com/example/demo/entity/Employee.java
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -3,15 +3,12 @@ package com.example.demo.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
 public class Employee {
+
+    public Employee() {
+    }
 
     @Id
     @GeneratedValue
