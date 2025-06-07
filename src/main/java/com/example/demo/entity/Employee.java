8d13945c0ba1060e8173a5b005eb85de1955e2d5
mutyala517 <mutyala.dharmendra@gmail.com>
2025-06-07 17:53:02 -0500

    Removing unused dependencies

--- src/main/java/com/example/demo/entity/Employee.java
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -16,6 +16,39 @@ public class Employee {
     @Id
     @GeneratedValue
     private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
     private String name;
     private String department;
     private Double salary;
