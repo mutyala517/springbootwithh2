01daf9d8d6597972fc372407d6181301ec30803d
mutyala517 <mutyala.dharmendra@gmail.com>
2025-09-27 12:19:30 -0500

    Updated address

--- src/main/java/com/example/demo/config/DataLoadingIntoDB.java
+++ src/main/java/com/example/demo/config/DataLoadingIntoDB.java
@@ -20,7 +20,7 @@ public class DataLoadingIntoDB implements CommandLineRunner {
         System.out.println("Loading initial data into the database...");
         // Example: Load some default employees or other entities
 
-        Employee employee1 = new Employee(new Address("1222 Hidden Ridge", "Irving", "75038", "Texas"), 6000.00, "IT", "Dharmendra Mutyala");
+        Employee employee1 = new Employee(new Address("Hidden Ridge", "Irving", "75038", "Texas"), 6000.00, "IT", "Dharmendra Mutyala");
         employeeRepository.save(employee1);
     }
 }
